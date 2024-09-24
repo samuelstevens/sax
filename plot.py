009dc85d3456c22eadb40954735f8c2ad59e7b91
Samuel Stevens <samuel.robert.stevens@gmail.com>
2024-09-24 16:25:39 -0400

    Update plot to use pareto frontier

--- plot.py
+++ plot.py
@@ -1,6 +1,7 @@
 import logging
 
 import beartype
+from jaxtyping import jaxtyped, Float, Array, Bool
 import tyro
 
 
@@ -8,11 +9,32 @@ log_format = "[%(asctime)s] [%(levelname)s] [%(name)s] %(message)s"
 logging.basicConfig(level=logging.INFO, format=log_format)
 logger = logging.getLogger("launch")
 
+colors = ["tab:blue", "tab:red", "tab:green", "tab:purple", "tab:orange"]
+
+
+@jaxtyped(typechecker=beartype.beartype)
+def is_pareto_efficient(points: Float[Array, "n 2"]) -> Bool[Array, " n"]:
+    """ """
+    import jax.numpy as jnp
+
+    # Sort points by x-value.
+    i_sorted = jnp.argsort(points[:, 0])
+    points = points[i_sorted]
+    is_efficient = jnp.zeros(len(points), dtype=bool)
+    min_y = jnp.inf
+    for i, (x, y) in enumerate(points):
+        if y < min_y:
+            min_y = y
+            is_efficient = is_efficient.at[i].set(True)
+
+    # Un-sort is_efficient to match original points order.
+    undo = jnp.zeros(len(points), dtype=int).at[i_sorted].set(jnp.arange(len(points)))
+    return is_efficient[undo]
+
 
 @beartype.beartype
-def plot_exp(ax, name: str):
+def plot_exp(ax, name: str, color: str):
     import jax.numpy as jnp
-    import scipy.spatial
     import sax.tracking
 
     exps = list(sax.tracking.load_by_tag(name))
@@ -20,28 +42,28 @@ def plot_exp(ax, name: str):
         jnp.array([[exp.data["val_l0"], exp.data["val_loss"]] for exp in exps]), axis=0
     )
 
-    chull = scipy.spatial.ConvexHull(points)
-    chull_points = points[chull.vertices]
+    if len(points) > 2:
+        mask = is_pareto_efficient(points)
+        pareto_points = points[mask]
+        other_points = points[~mask]
 
-    mask = jnp.ones(len(points), dtype=bool)
-    other_points = points[mask.at[chull.vertices].set(False)]
+        ax.scatter(
+            other_points[:, 0],
+            other_points[:, 1],
+            alpha=0.2,
+            label=f"{name} (hparam. sweep)",
+            color=color,
+        )
 
-    ax.scatter(
-        other_points[:, 0],
-        other_points[:, 1],
-        alpha=0.2,
-        label=f"{name} (hparam. sweep)",
-        color="tab:blue",
-    )
-
-    chull_points = chull_points[chull_points[:, 0].argsort()]
-    ax.plot(
-        chull_points[:, 0],
-        chull_points[:, 1],
-        color="tab:blue",
-        label=f"{name} (optimal)",
-        marker="o",
-    )
+        ax.plot(
+            pareto_points[:, 0],
+            pareto_points[:, 1],
+            color=color,
+            label=f"{name} (optimal)",
+            marker="o",
+        )
+    else:
+        ax.scatter(points[:, 0], points[:, 1], label=f"{name} (all)", color=color)
 
 
 @beartype.beartype
@@ -56,6 +78,11 @@ def main(exp_names: list[str], /, out: str):
         exp_names: list of experiment names to plot.
         out: filename to save plot to.
     """
+
+    if len(exp_names) > len(colors):
+        print(f"Add more colors to {__file__}.")
+        return
+
     import matplotlib.pyplot as plt
     import matplotlib.ticker
 
@@ -66,8 +93,8 @@ def main(exp_names: list[str], /, out: str):
     # 3. Make a scatter plot.
     fig, ax = plt.subplots()
 
-    for name in exp_names:
-        plot_exp(ax, name)
+    for name, color in zip(exp_names, colors):
+        plot_exp(ax, name, color)
 
     ax.set_title("L0 vs Loss")
     ax.set_xscale("log")
@@ -79,6 +106,7 @@ def main(exp_names: list[str], /, out: str):
     ax.yaxis.set_major_formatter(matplotlib.ticker.FuncFormatter(ticker_fn))
     ax.xaxis.set_major_formatter(matplotlib.ticker.FuncFormatter(ticker_fn))
 
+    fig.tight_layout()
     fig.savefig(out)
 
 
